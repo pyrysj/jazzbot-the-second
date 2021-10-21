34e944f75090cf072bd952568435d34d926499fe
pyrysj <pyry.sjovall@outlook.com>
2021-10-21 19:55:33 +0300

    Adding barebones functionality to the player file

--- musicPlayer.js
+++ musicPlayer.js
@@ -1,6 +1,22 @@
+const ytdl = require('ytdl-core');
+const search = require('youtube-search');
+const { key } = require('./config.json');
+
+const opts = {
+	maxResults: 25,
+	type: 'video',
+	videoDuration: 'long',
+	key: key,
+};
+
+// using just 0 for now, more fancy features could eventually be implemented
+
 // initialises a class
 module.exports = class MusicPlayer {
 	test() {
 		console.log('hello world!');
+	}
+	play() {
+		console.log('TBI');
 	}
 };
