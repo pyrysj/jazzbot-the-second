adde0ffd3badab1aa21170b89dc7a871452ef026
pyrysj <pyry.sjovall@outlook.com>
2021-10-27 23:56:07 +0300

    added more to the play command, early days

--- commands/play.js
+++ commands/play.js
@@ -1,12 +1,34 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { VoiceConnectionStatus, AudioPlayerStatus, getVoiceConnection, AudioPlayer } = require('@discordjs/voice');
+// const { ExplicitContentFilterLevels } = require('discord.js/typings/enums');
 const { mPlayer } = require('../index.js');
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('play')
 		.setDescription('Command that makes jazzy tunes play.'),
+	// First checks if user in voice channel. We do this by using a try catch statemnet
+	// if the connection exists <- no error, if it doesn't <- error
 	async execute(interaction) {
-		mPlayer.test();
-		return interaction.reply('Pong!');
+		try {
+			const connection = getVoiceConnection(interaction.member.voice.channelId);
+			const subscription = connection.subscribe(AudioPlayer);
+			mPlayer.play();
+		}
+		catch (error) {
+			interaction.reply('You need to be in a voice call to listen to jazzy tunes.');
+		}
+		// if (interaction.member.voice.channelId) {
+		//	console.log('works!');
+		//	mPlayer.play();
+		// }
+		// else {
+		//	console.log('yup it goes into the else bracket!');
+		// }
+		// interaction.reply('You need to be in a voice call to listen to jazzy tunes.');
+		// interaction.user.g
+		// const connection = getVoiceConnection();
+
 	},
 };
