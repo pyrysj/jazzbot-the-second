5913687ce9fcbca059c16afe9b9f0b95939094dc
pyrysj <pyry.sjovall@outlook.com>
2021-10-31 23:30:17 +0200

    reverted changes because code broke completely

--- player.ts
+++ player.ts
@@ -1,8 +1,11 @@
+import { VoiceConnection } from "@discordjs/voice";
 // had to move to typescript, will hopefully be cleaned
 // up in future revisions, right now a pain to look through
 export class Player{
     searchArg: string;
-    constructor(searchArg: string) {
+    connection : VoiceConnection;
+    constructor(searchArg: string, connection: VoiceConnection) {
         this.searchArg = searchArg;
+        this.connection = connection;
     }
 }
