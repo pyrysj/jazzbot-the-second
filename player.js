fae5497df6057154ae007447ea2fd5f151b1fed3
pyrysj <pyry.sjovall@outlook.com>
2021-11-09 01:14:43 +0200

    Added partial implementation of voice, work needed

--- player.js
+++ player.js
@@ -1,61 +1,53 @@
-import { createAudioPlayer, VoiceConnection, createAudioResource } from "@discordjs/voice";
-import ytdl from 'ytdl-core';
-import youtubeSearch from "youtube-search";
-import config = require('./config.json');
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Player = void 0;
+const voice_1 = require("@discordjs/voice");
+const youtube_search_1 = __importDefault(require("youtube-search"));
+const config = require("./config.json");
 // had to move to typescript, will hopefully be cleaned
 // up in future revisions, right now a pain to look through
-
 // NB: means the audio player, not "our" player!
 // will eventually be moved to another file? mabye? with customisations and 
 // everything
-const player = createAudioPlayer();
-
+const player = (0, voice_1.createAudioPlayer)();
 // need to think about where to "tidily" store these options, mabye as a .json?
 // for now just something to get us going -- will eventually be made more sophisticated!
-var opts: youtubeSearch.YouTubeSearchOptions = {
+var opts = {
     maxResults: 10,
     key: config.key
-}
-
+};
 // tidy function to search and return the url of a result
-
-
-export class Player {
-    searchArg: string;
-    connection : VoiceConnection;
-    songURL: string;
-
-    constructor(searchArg: string, connection: VoiceConnection) {
+class Player {
+    constructor(searchArg, connection) {
         this.searchArg = searchArg;
         this.connection = connection;
-        this.songURL = ""
+        this.songURL = "";
     }
-
     // function to play songs
-    play(): void {
+    play() {
         // just a basic player for now, does as described really
         this.search("jazz");
         //onst subscription = this.connection.subscribe(player);
         //const stream = ytdl(this.songURL, { filter: 'audioonly', dlChunkSize: 0 });
         //const dispatcher = player.play(createAudioResource(stream));
     }
-
-    private search(searchArg: string): any {
-        let url = youtubeSearch(searchArg, opts, (err, results) => {
-            if(err) return console.log(err);
-            if (results){
+    search(searchArg) {
+        let url = (0, youtube_search_1.default)(searchArg, opts, (err, results) => {
+            if (err)
+                return console.log(err);
+            if (results) {
                 return results[0].link;
             }
-    
         });
-        console.log(url)
-
+        console.log(url);
     }
-
     // function to eventually show currently playing song
     // NB: will need to be coded in as a command, etc etc§
-    nowPlaying(): void {
+    nowPlaying() {
         console.log('TBI');
     }
 }
-
+exports.Player = Player;
--- trackManager.js
+++ trackManager.js
@@ -1,9 +1,12 @@
+"use strict";
 // some notes here -- gonna use the magic of the discord api to track events
 // when a stream pauses -- aka when a song stops, meaning we can try playing
 // the next song & if no song is there -- we wait or something 
-
-export class Track {
-    constructor(){
-        console.log("to be implementeed")
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Track = void 0;
+class Track {
+    constructor() {
+        console.log("to be implementeed");
     }
 }
+exports.Track = Track;
