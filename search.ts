f6f63cc4b75f4841cebc252dc23754a922923dff
pyrysj <pyry.sjovall@outlook.com>
2021-11-12 00:39:47 +0200

    cleanup

--- search.ts
+++ search.ts
@@ -5,8 +5,10 @@ import config = require('./config.json');
 // there are probably better ways & ways that increase modularity
 // but for a simple discord bot, even this is kinda overkill haha
 export class Search {
+    videoUrl: string
     constructor(){
-
+        // here for  future uses, might be needed
+        this.videoUrl = ""; 
     }
     public async yt_search(searchArg: string): Promise<string>{
         return new Promise<string>((resolve) => {
@@ -19,8 +21,9 @@ export class Search {
                 }
                 })
                 // for now just returns the first video id 
+                // can be made more fancy should ever a need arise
                 .then(function (response) {
-                    resolve(response.data.items[1].id.videoId);
+                    resolve(`https://www.youtube.com/watch?v=${response.data.items[1].id.videoId}`);
                   })
                 .catch(function (error) {
                     console.log(error);
